0db870bba9c34707e4e261e4e8882172588ddc88
Shweta Shendage <ShwetaShendage@Shrikants-MacBook-Air.local>
2018-05-21 13:11:06 +0530

    Reachebility class added for internet connection check

---
+++ Podfile
@@ -0,0 +1,19 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'ProofOfConcept' do
+
+
+pod 'ReachabilitySwift'
+
+  target 'ProofOfConceptTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ProofOfConceptUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- ProofOfConcept/Controller/POCFactsTableViewController.swift
+++ ProofOfConcept/Controller/POCFactsTableViewController.swift
@@ -7,26 +7,69 @@
 //
 
 import UIKit
+import Reachability
 
 class POCFactsTableViewController: UITableViewController {
   
   let service = POCServiceClass()
+  var factsArray: [Any] = []
+  let reachability = Reachability()!
   
-  //    MARK: - Lifecycle
+  //      MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     
     self.title = "POC"
     
-    //    Call JSON Feed
-    service.getFacts()
+    self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    self.tableView.rowHeight = UITableViewAutomaticDimension
+    self.tableView.estimatedRowHeight = 120
+    self.tableView.separatorInset = .zero
+    self.tableView.accessibilityIdentifier = "factsTableView"
+    
+    // Call JSON Feed
+    getData()
     
   }
-  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
   
+  //  MARK: - Public Methods
+  func getData() {
+    if reachability.connection == .none{
+      self.showAlertWith(title: "Message", message: "No Internet connection.")
+    }
+    else{
+      //    Call Service
+      service.getFacts()
+    }
+  }
+  
+  func showAlertWith(title: String, message: String, style: UIAlertControllerStyle = .alert) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+    let action = UIAlertAction(title: "Ok", style: .default) { (action) in
+      self.dismiss(animated: true, completion: nil)
+    }
+    alertController.addAction(action)
+    self.present(alertController, animated: true, completion: nil)
+  }
+  
 }
 
+//    MARK: - UITableViewDataSource
+extension POCFactsTableViewController {
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.factsArray.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    return cell
+    
+  }
+  
+}
 
